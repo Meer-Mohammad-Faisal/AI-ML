0def04657224a28745fbd690d60aa7b1f1a96426
MEER MOHAMMAD FAISAL <fmd35585@gmail.com>
2025-11-08 18:35:58 +0530

    up

--- 07_Machine_learning/03_Supervised_Logistic_Reg/2_Final_project/app.py
+++ 07_Machine_learning/03_Supervised_Logistic_Reg/2_Final_project/app.py
@@ -19,7 +19,7 @@ except ImportError:
 @st.cache_resource
 def load_model():
     try:
-        model = joblib.load("KNN_heart.pkl")
+        model = joblib.load("knn_heart.pkl")
         scaler = joblib.load("scaler.pkl")
         expected_columns = joblib.load("columns.pkl")
         return model, scaler, expected_columns
